824ea84706069120a3cc6699c53f13a45df40bea
LeHoangHai0612 <hai2005b6@gmail.com>
2024-10-22 20:29:43 +0700

    Add files via upload

---
+++ BTL1/src/main/java/com/mycompany/quanlydemo1/Quanlydemo1.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.quanlydemo1;
+
+public class Quanlydemo1 {
+
+    public static void main(String[] args) {
+        // gọi jv swing 
+        JFQuanly jfs = new JFQuanly();
+       jfs.show();
+    }
+}
